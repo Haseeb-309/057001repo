09bdd79ef8a1578a502540018c0485f8664df150
Haseeb-309 <haseebch173@gmail.com>
2024-09-27 19:42:46 +0500

    price function added

--- Product_stubs.py
+++ Product_stubs.py
@@ -2,10 +2,18 @@ class Product:
   def __init__(self, name, amount, price):
       self.name = name
       self.amount = amount
-      self.price = price\
+      self.price = price
       
   def get_price(self, quantity):
-      pass
+     if quantity < 10:
+        unit_price = self.price
+     if quantity >= 10 and quantity < 50:
+        unit_price = self.price * 0.8
+     if quantity >= 50:
+        unit_price = self.price * 0.5
+
+     product_price = quantity * unit_price
+     return product_price
     
 
   def make_purchase(self, quantity):
